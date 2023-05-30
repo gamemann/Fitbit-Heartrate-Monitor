13269f7e4135d9cf9b526eacac4ff3491ad79c14
Christian Deacon <christian@moddingcommunity.com>
2023-05-30 00:25:56 +0000

    Add support for timeout and catch exception in-case.

--- src/actions/http.py
+++ src/actions/http.py
@@ -1,11 +1,15 @@
 import requests
 
-def send_http_request(url, method="POST", headers={}, body={}):
+def send_http_request(url, method="POST", headers={}, body={}, timeout=5.0):
     resp = None
 
-    if method.lower() == "post":
-        resp = requests.post(url, data=body, headers=headers)
-    else:
-        resp = requests.get(url, data=body, headers=headers)
+    try:
+        if method.lower() == "post":
+            resp = requests.post(url, data=body, headers=headers, timeout=timeout)
+        else:
+            resp = requests.get(url, data=body, headers=headers)
+    except Exception as e:
+        print("Failed to send HTTP request.")
+        print(e)
 
     return resp
